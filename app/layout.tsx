70644c68815ed08bb2f1619a0e917f6274dad0de
shivams9721 <shivam.raj_cs21@gla.ac.in>
2026-04-06 15:48:48 +0530

    feat: update favicon to Creative-Kid's--Logo.jpg.jpeg

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata = {
     type: 'website',
   },
   icons: {
-    icon: '/images/Creative-Kid\'s---Logo (1).png',
+    icon: '/images/Creative-Kid\'s--Logo.jpg.jpeg',
   },
 }
 
