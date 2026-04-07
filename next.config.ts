e86048e3850dadb4f50b6540c6dc56ace97dc2a5
shivams9721 <shivam.raj_cs21@gla.ac.in>
2026-04-07 15:47:34 +0530

    fix : image change

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**.amazonaws.com" },
-      { protocol: "https", hostname: "i.postimg.cc" },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
 };
